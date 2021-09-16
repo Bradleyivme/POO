ca4dbdebc86df2991745464dd26e1396ef8b5c17
Bradley Jacinto <bradleyivme@gmail.com>
2021-09-16 11:33:11 -0600

    Programa terminado.

---
+++ src/main/java/com/mycompany/poo/Heroe.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.poo;
+
+public class Heroe extends Personaje implements Guerrero{
+
+    @Override
+    public void Modificarvida(){
+        
+    }
+    
+    @Override
+    public void atacar() {
+        System.out.println("El Héroe ataca.");
+    }
+}
